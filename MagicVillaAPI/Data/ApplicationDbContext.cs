425cf0a187ecf97d767df7c19ba25528913e052b
Umang Shrestha <umangstha124@gmail.com>
2023-11-27 17:36:22 +0545

    Dependency Injection and DBContext

---
+++ MagicVillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MagicVillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
